b07c4dac147f38d5cec6f4a19ca210ea5ec178ef
Moore <derekmoore@derek-jr-jr.metagenics.com>
2018-12-04 17:10:38 -0800

    updated content for app build and requirements

---
+++ assets/javascript/app.js
@@ -0,0 +1,32 @@
+// create an array of strings, each one related to a topic that interests me
+
+// App will take in the topics in this array and create buttons in HTML(Which one is this?)
+
+// When buttons are clicked 10 static (non-animated buttons (Paused.html)) will display.
+
+//When user clicks a still image it will start to animate
+
+// Under every GIF display rating (Giphy AIP)
+
+// Add form to page that takes in User data and places it into your 'topics' array. Then make a function that makes a new button based on their input
+
+// ==== IMPORTANT ==== 
+
+// Create a ReadMe.md file for this project
+
+// Add this to your portfolio to get full credit on the project.
+
+//  -----  BONUS ROUND  ------
+
+// make it fully mobile responsive
+
+// allow additional GIFs to be added to the page. NOT override existing ones
+
+// List extra metadata. From API
+
+// Include 1 touch download button
+
+// Integrate this with additional AIP's (OMDB, Bands in Town).
+
+// Allow a 'favorites' option to a GIF
+

ee997aa9dd66e2818793051bdf7d70814b1c0b05
Derek Moore <moore8577@gmail.com>
2018-12-05 21:30:53 -0800

    generated gifs, paused them. Stuck on Play feature. Added static form

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,98 @@
-// create an array of strings, each one related to a topic that interests me
+// A $( document ).ready() block.
+$(document).ready(function () {
+    console.log("ready!");
 
-// App will take in the topics in this array and create buttons in HTML(Which one is this?)
 
-// When buttons are clicked 10 static (non-animated buttons (Paused.html)) will display.
+    // create an array of strings, each one related to a topic that interests me
+    var cookieArr = ["Brown Butter", "Snickerdoodle", "Chocolate Chip", "Fortune Cookie", "Sugar Cookie", "Oreo", "Gingerbread Cookie", "Gingersnap Cookie", "Molasses Cookie", "Oatmeal Cookie"];
+    // App will take in the topics in this array and create buttons in HTML(Which one is this?)
+    function setDefaultButtons() {
+        for (var i = 0; i < cookieArr.length; i++) {
+            var arrBtn = $("<button>");
+            arrBtn.addClass("btn btn-primary");
+            // Pojo
+            arrBtn.attr({ "type": "button", "cookie-type": cookieArr[i] });
+            arrBtn.text(cookieArr[i]);
+            $(".buttons").append(arrBtn);
 
-//When user clicks a still image it will start to animate
+        }
+    }
+    setDefaultButtons();
+    // When buttons are clicked 10 static (non-animated buttons (Paused.html)) will display.
+    $("button").on("click", function () {
+        var cookieType = $(this).attr("cookie-type");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            cookieType + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-// Under every GIF display rating (Giphy AIP)
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+                console.log(response);
+                var results = response.data;
 
-// Add form to page that takes in User data and places it into your 'topics' array. Then make a function that makes a new button based on their input
+                for (var i = 0; i < results.length; i++) {
+                    var cookieDiv = $("<div class='item'>");
 
-// ==== IMPORTANT ==== 
+                    var rating = results[i].rating;
 
-// Create a ReadMe.md file for this project
+                    var p = $("<p>").text("Rating: " + rating);
 
-// Add this to your portfolio to get full credit on the project.
+                    var cookieImage = $("<img>");
+                    cookieImage.addClass("gif");
+                    cookieImage.attr({
+                        'src': results[i].images.fixed_height_still.url,
+                        'animated-gif': results[i].images.fixed_height.url,
+                        'data-state': 'still',
+                        'still-gif': results[i].images.fixed_height_still.url
+                    })
 
-//  -----  BONUS ROUND  ------
+                    cookieDiv.prepend(p);
+                    cookieDiv.prepend(cookieImage);
 
-// make it fully mobile responsive
+                    $(".gif-go-here").prepend(cookieDiv);
 
-// allow additional GIFs to be added to the page. NOT override existing ones
+                }
+            });
+    });
+    //When user clicks a still image it will start to animate
 
-// List extra metadata. From API
+   
+    $(".gif").on("click", function () {
+        console.log("click event");
+        var state = $(this).attr("data-state");
+        console.log(state);
+        if (state === "still") {
+            $(this).attr("src", $(this).attr('animated-gif'));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("still-gif"));
+            $(this).attr("data-state", "still");
+        }
+    });
+    // Under every GIF display rating (Giphy AIP)
+    // DONE ABOVE
+    // Add form to page that takes in User data and places it into your 'topics' array. Then make a function that makes a new button based on their input
 
-// Include 1 touch download button
+    // ==== IMPORTANT ==== 
 
-// Integrate this with additional AIP's (OMDB, Bands in Town).
+    // Create a ReadMe.md file for this project
 
-// Allow a 'favorites' option to a GIF
+    // Add this to your portfolio to get full credit on the project.
 
+    //  -----  BONUS ROUND  ------
+
+    // make it fully mobile responsive
+
+    // allow additional GIFs to be added to the page. NOT override existing ones
+
+    // List extra metadata. From API
+
+    // Include 1 touch download button
+
+    // Integrate this with additional AIP's (OMDB, Bands in Town).
+
+    // Allow a 'favorites' option to a GIF
+
+});
